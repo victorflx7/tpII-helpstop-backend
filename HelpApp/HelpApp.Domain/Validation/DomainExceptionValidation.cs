e7c902941cccacca51baa9edfb63f0824fa83180
victorflx7 <victorsantosflx@gmail.com>
2025-02-24 12:34:18 -0300

    Criação Básica dos domínios de entidades.

---
+++ HelpApp/HelpApp.Domain/Validation/DomainExceptionValidation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelpApp.Domain.Validation
+{
+    internal class DomainExceptionValidation : Exception
+    {
+        public DomainExceptionValidation(string error) : base(error) { }
+
+        public static void When(bool hasError, string error)
+        {
+            if (hasError)
+            {
+                throw new DomainExceptionValidation(error);
+            }
+        }
+    }
+}
