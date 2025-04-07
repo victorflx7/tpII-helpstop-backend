96a5296a9f8e571682c937622f3af675f17a0eb4
victorflx7 <victorsantosflx@gmail.com>
2025-04-07 12:03:11 -0300

    Alterações novas

--- HelpApp/HelpApp.Domain.Test/CategoryUnitTest.cs
+++ HelpApp/HelpApp.Domain.Test/CategoryUnitTest.cs
@@ -1,6 +1,7 @@
 ﻿using HelpApp.Domain.Entities;
 using FluentAssertions;
 using Xunit;
+using System.Xml.Linq;
 
 namespace HelpApp.Domain.Test
 {
@@ -10,18 +11,34 @@ namespace HelpApp.Domain.Test
         [Fact(DisplayName = "Create Category With Valid State")]
         public void CreateCategory_WithValidParameters_ResultObjectValidState()
         {
-            Action action = () => new Category(1, "Category Name");
+            Action action = () => new Category(1, "Categoria ");
             action.Should().NotThrow<HelpApp.Domain.Validation.DomainExceptionValidation>();
         }
         #endregion
         #region Testes Negativos
-        [Fact(DisplayName ="Create Category With Name Empty")]
+        [Fact(DisplayName = "Create Category With Alone Name")]
         public void CreateCategory_WithNameEmpty_ResultObjetcException()
         {
             Action action = () => new Category(1, "");
             action.Should().Throw<HelpApp.Domain.Validation.DomainExceptionValidation>()
                 .WithMessage("Invalid name, name is required.");
         }
-        #endregion
+
+        [Fact(DisplayName = "Create Category With Id Invalid")]
+        public void CreateCategory_WithIdInvalid_ResultObjetcException()
+        {
+            Action action = () => new Category(-1, "");
+            action.Should().Throw<HelpApp.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid Id value.");
+        }
+        [Fact(DisplayName = "Create Category With Too Short")]
+        public void CreateCategory_WithTooShort_ResultObjetcException()
+        {
+            Action action = () => new Category(1, "ca");
+            action.Should().Throw<HelpApp.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name, too short, minimum 3 characters.");
+        }
+
+            #endregion
+        }
     }
-}
